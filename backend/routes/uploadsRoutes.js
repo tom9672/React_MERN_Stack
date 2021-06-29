b928f0af42a5091adc1e69abae48cb3d54572c45
tom9672 <18896825080@163.com>
2021-06-29 21:28:21 +0800

    backend: uploads images

---
+++ backend/routes/uploadsRoutes.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import multer from 'multer'
+import path from 'path'
+const router = express.Router()
+
+// create disk save engine
+const storage=multer.diskStorage({
+    destination(req, file, cb) {
+        cb(null,'uploads/')},
+    filename(req, file,cb){
+        cb(null,`${file.fieldname} - ${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+// check file type
+const checkFileType=(file,cb) =>{
+    const fileTypes = /jpg||jepg||png/
+    const extname = fileTypes.test(path.extname(file.originalname).toLowerCase())
+    const mimetype = fileTypes.test(file.mimetype)
+    if(mimetype && extname){
+        return cb(null,true)
+    }else{
+        return cb('Only images allow')
+    }
+}
+
+// upload file
+const upload = multer({storage, fileFilter:function(req,file,cb){
+    checkFileType(file,cb)
+}})
+
+router.post('/', upload.single('image'), (req,res)=>{
+    res.send(`/${req.file.path}`)
+})
+
+export default router
--- backend/server1.js
+++ backend/server1.js
@@ -6,6 +6,9 @@ import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from './routes/uploadsRoutes.js'
+import path from 'path'
+
 dotenv.config();
 connectDB();
 
@@ -15,6 +18,10 @@ app.use(express.json());
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound);
 app.use(errorHandler);
